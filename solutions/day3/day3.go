16d09ff4d00f19ecd3b8cbb2c2fe762ce322bbd2
Shajesh Jegatheeswaran <shajesh.14@gmail.com>
2022-12-04 19:15:20 -0500

    Day 3 cleanup... we don't error check :coolguns:

--- solutions/day3/day3.go
+++ solutions/day3/day3.go
@@ -2,36 +2,16 @@ package day3
 
 import (
 	"embed"
-	"errors"
-	"fmt"
-	"strings"
 
+	"github.com/ShajeshJ/adventofcode_2022/common/util"
 	"go.uber.org/zap"
 )
 
 //go:embed part1.txt
 var files embed.FS
 
-func getPartOneData() (data []string, err error) {
-	bytes, err := files.ReadFile("part1.txt")
-	if err != nil {
-		return
-	}
-
-	data = strings.Split(string(bytes), "\n")
-	return
-}
-
-func getPartTwoData() (data [][]string, err error) {
-	rucksacks, err := getPartOneData()
-	if err != nil {
-		return
-	}
-
-	if len(rucksacks)%3 != 0 {
-		err = errors.New("cannot evenly divide elves into groups of 3")
-		return
-	}
+func getPartTwoData() (data [][]string) {
+	rucksacks := util.ReadProblemInput(files, 1)
 
 	for i := 0; i < len(rucksacks); i += 3 {
 		data = append(data, []string{rucksacks[i], rucksacks[i+1], rucksacks[i+2]})
@@ -40,107 +20,52 @@ func getPartTwoData() (data [][]string, err error) {
 	return
 }
 
-func getCommonLetter(strs ...string) (rune, error) {
-	if len(strs) <= 1 {
-		return 0, errors.New("must give > 1 strings to find the common letter")
-	}
+func getCommonLetter(strs ...string) rune {
+	overlapCount := map[rune]int{}
 
-	runningOverlaps := map[rune]bool{}
-
-	for _, r := range strs[0] {
-		runningOverlaps[r] = true
-	}
-
-	for _, str := range strs[1:] {
-		prevOverlaps := runningOverlaps
-		runningOverlaps = map[rune]bool{}
-
+	for _, str := range strs[:len(strs)-1] {
 		for _, r := range str {
-			if _, ok := prevOverlaps[r]; ok {
-				runningOverlaps[r] = true
-			}
+			overlapCount[r]++
 		}
 	}
 
-	var overlaps []string
-	for r := range runningOverlaps {
-		overlaps = append(overlaps, string(r))
+	for _, r := range strs[len(strs)-1] {
+		if overlapCount[r] == len(strs)-1 {
+			return r
+		}
 	}
 
-	if len(overlaps) == 0 {
-		return 0, fmt.Errorf(`no overlap found across strings %v`, strs)
-	}
-
-	if len(overlaps) > 1 {
-		return 0, fmt.Errorf(`multiple overlaps %v found in %v`, overlaps, strs)
-	}
-
-	return rune(overlaps[0][0]), nil
+	return 0
 }
 
-func getPriority(r rune) (int, error) {
+func getPriority(r rune) int {
 	if 'a' <= r && r <= 'z' {
-		return int(r - 'a' + 1), nil
+		return int(r - 'a' + 1)
+	} else { //'A' <= r && r <= 'Z'
+		return int(r - 'A' + 27)
 	}
-
-	if 'A' <= r && r <= 'Z' {
-		return int(r - 'A' + 27), nil
-	}
-
-	return 0, fmt.Errorf("rune %v (%v) cannot be prioritized", string(r), r)
 }
 
 func PartOne(logger *zap.SugaredLogger) {
-	rucksacks, err := getPartOneData()
-	if err != nil {
-		logger.Error(err)
-		return
-	}
+	rucksacks := util.ReadProblemInput(files, 1)
 
 	total := 0
 
 	for _, items := range rucksacks {
-		common, err := getCommonLetter(items[:len(items)/2], items[len(items)/2:])
-		if err != nil {
-			logger.Error(err)
-			return
-		}
-
-		priority, err := getPriority(common)
-		if err != nil {
-			logger.Error(err)
-			return
-		}
-
-		total += priority
+		rucksackCompartments := []string{items[:len(items)/2], items[len(items)/2:]}
+		total += getPriority(getCommonLetter(rucksackCompartments...))
 	}
 
 	logger.Info(total)
 }
 
 func PartTwo(logger *zap.SugaredLogger) {
-	groups, err := getPartTwoData()
-	if err != nil {
-		logger.Error(err)
-		return
-	}
+	groups := getPartTwoData()
 
 	total := 0
 
 	for _, group := range groups {
-		common, err := getCommonLetter(group...)
-		if err != nil {
-			logger.Error(err)
-			return
-		}
-
-		priority, err := getPriority(common)
-		if err != nil {
-			logger.Error(err)
-			return
-		}
-
-		total += priority
+		total += getPriority(getCommonLetter(group...))
 	}
 
 	logger.Info(total)
