92547fb87391806ab18719a39f2aa49ecfa13879
Shajesh Jegatheeswaran <shajesh.14@gmail.com>
2022-12-16 00:03:37 -0500

    Complete day 15 part 1, and brute force attempt for part 2

---
+++ solutions/day15/day15.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"regexp"
+	"sync"
+
+	"github.com/ShajeshJ/adventofcode_2022/common/logging"
+	"github.com/ShajeshJ/adventofcode_2022/common/util"
+)
+
+var log = logging.GetLogger()
+
+//go:embed part1.txt
+var files embed.FS
+
+type State int
+
+const (
+	Unknown = iota
+	HasBeacon
+	HasSensor
+	NoBeacon
+)
+
+type Sensor struct {
+	Point         []int
+	Beacon        []int
+	NoBeaconRange int
+}
+
+func ManhattanDist(p1, p2 []int) int {
+	return util.Abs(p1[0]-p2[0]) + util.Abs(p1[1]-p2[1])
+}
+
+func getPartOneData() []Sensor {
+	xregex := regexp.MustCompile(`x=(-?\d+)`)
+	yregex := regexp.MustCompile(`y=(-?\d+)`)
+
+	var sensors []Sensor
+	for _, line := range util.ReadProblemInput(files, 1) {
+		xpoints := xregex.FindAllStringSubmatch(line, 2)
+		ypoints := yregex.FindAllStringSubmatch(line, 2)
+		s := Sensor{
+			Point:  []int{util.AtoiNoError(xpoints[0][1]), util.AtoiNoError(ypoints[0][1])},
+			Beacon: []int{util.AtoiNoError(xpoints[1][1]), util.AtoiNoError(ypoints[1][1])},
+		}
+		s.NoBeaconRange = ManhattanDist(s.Point, s.Beacon)
+		sensors = append(sensors, s)
+	}
+	return sensors
+}
+
+func PartOne() any {
+	sensors := getPartOneData()
+	targetY := 2_000_000
+	atTargetY := map[int]State{}
+
+	for _, s := range sensors {
+		if s.Point[1] == targetY {
+			atTargetY[s.Point[0]] = HasSensor
+		}
+		if s.Beacon[1] == targetY {
+			atTargetY[s.Beacon[0]] = HasBeacon
+		}
+
+		// sub the Y distance, and focus only on X
+		beaconlessRange := s.NoBeaconRange - util.Abs(s.Point[1]-targetY)
+
+		for x := s.Point[0] - beaconlessRange; x <= s.Point[0]+beaconlessRange; x++ {
+			if atTargetY[x] == Unknown {
+				atTargetY[x] = NoBeacon
+			}
+		}
+	}
+
+	noBeaconCount := 0
+	for _, s := range atTargetY {
+		if s == NoBeacon || s == HasSensor {
+			noBeaconCount++
+		}
+	}
+	return noBeaconCount
+}
+
+func CanHaveDistressBeacon(x, y int, sensors []Sensor) bool {
+	for _, s := range sensors {
+		if (s.Point[0] == x && s.Point[1] == y) ||
+			(s.Beacon[0] == x && s.Beacon[1] == y) ||
+			ManhattanDist(s.Point, []int{x, y}) <= s.NoBeaconRange {
+			return false
+		}
+	}
+	return true
+}
+
+func PartTwo() any {
+	sensors := getPartOneData()
+	maxCoords := 4_000_000
+
+	distressX, distressY := -1, -1
+	var wg sync.WaitGroup
+	// pick up from x=1461600
+	for x := 0; x <= maxCoords; x++ {
+		x := x
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for y := 0; y <= maxCoords; y++ {
+				if CanHaveDistressBeacon(x, y, sensors) {
+					distressX, distressY = x, y
+					log.Infof("Early exit value: %v", distressX*4_000_000+distressY)
+					break
+				}
+			}
+		}()
+
+		if x%400 == 0 {
+			log.Infof("waiting for batch x=%v", x)
+			wg.Wait()
+		}
+
+		if distressX != -1 {
+			break
+		}
+	}
+
+	return distressX*4_000_000 + distressY
+}
+
+func main() {
+	log.Infow(fmt.Sprintf("Answer: %v", PartOne()), "part", 1)
+	log.Infow(fmt.Sprintf("Answer: %v", PartTwo()), "part", 2)
+}
---
+++ solutions/day15_copy/day15_copy.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"regexp"
+
+	"github.com/ShajeshJ/adventofcode_2022/common/logging"
+	"github.com/ShajeshJ/adventofcode_2022/common/util"
+)
+
+var log = logging.GetLogger()
+
+//go:embed part1.txt
+var files embed.FS
+
+type State int
+
+const (
+	Unknown = iota
+	HasBeacon
+	HasSensor
+	NoBeacon
+)
+
+type Sensor struct {
+	Point  [2]int
+	Beacon [2]int
+}
+
+func getPartOneData() []Sensor {
+	xregex := regexp.MustCompile(`x=(-?\d+)`)
+	yregex := regexp.MustCompile(`y=(-?\d+)`)
+
+	var sensors []Sensor
+	for _, line := range util.ReadProblemInput(files, 1) {
+		xpoints := xregex.FindAllStringSubmatch(line, 2)
+		ypoints := yregex.FindAllStringSubmatch(line, 2)
+		sensors = append(sensors, Sensor{
+			Point:  [2]int{util.AtoiNoError(xpoints[0][1]), util.AtoiNoError(ypoints[0][1])},
+			Beacon: [2]int{util.AtoiNoError(xpoints[1][1]), util.AtoiNoError(ypoints[1][1])},
+		})
+	}
+	return sensors
+}
+
+func ManhattanDist(p1, p2 [2]int) int {
+	return util.Abs(p1[0]-p2[0]) + util.Abs(p1[1]-p2[1])
+}
+
+func PartOne() any {
+	sensors := getPartOneData()
+	targetY := 2_000_000
+	atTargetY := map[int]State{}
+
+	for _, s := range sensors {
+		if s.Point[1] == targetY {
+			atTargetY[s.Point[0]] = HasSensor
+		}
+		if s.Beacon[1] == targetY {
+			atTargetY[s.Beacon[0]] = HasBeacon
+		}
+
+		beaconlessRange := ManhattanDist(s.Point, s.Beacon)
+		beaconlessRange -= util.Abs(s.Point[1] - targetY) // sub the Y distance, and focus only on X
+
+		for x := s.Point[0] - beaconlessRange; x <= s.Point[0]+beaconlessRange; x++ {
+			if atTargetY[x] == Unknown {
+				atTargetY[x] = NoBeacon
+			}
+		}
+	}
+
+	noBeaconCount := 0
+	for _, s := range atTargetY {
+		if s == NoBeacon || s == HasSensor {
+			noBeaconCount++
+		}
+	}
+	return noBeaconCount
+}
+
+type Range struct {
+	Min, Max int
+}
+
+type SensorMap struct {
+	MaxLen int
+	// Covered ranges in order
+	// new ranges added that overlap with existing ones will be merged
+	Covered []Range
+}
+
+func NewSensorMap(squareLen int) SensorMap {
+	return SensorMap{MaxLen: squareLen, Covered: []Range{}}
+}
+
+func (sm *SensorMap) TrySet(x, miny, maxy int) {
+	if x < 0 || x >= sm.MaxLen {
+		return
+	}
+
+	if miny < 0 {
+		miny = 0
+	}
+	if maxy >= sm.MaxLen {
+		maxy = sm.MaxLen - 1
+	}
+
+	if miny > maxy {
+		return
+	}
+
+	// candidate range to insert
+	cr := Range{x*sm.MaxLen + miny, x*sm.MaxLen + maxy}
+
+	// reconstruct list of ranges, merging `cr` with existing ones where appropriate
+	newCovered := []Range{}
+	breakIndex := -1
+	for i, r := range sm.Covered {
+
+		if r.Max+1 < cr.Min {
+			// looped range is at least 1 space before the candidate
+			newCovered = append(newCovered, r)
+			continue
+		}
+
+		if cr.Max+1 < r.Min {
+			// looped range is at least 1 space after the candidate;
+			// `cr` is complete, and we can add unappended remainder
+			newCovered = append(newCovered, cr)
+			breakIndex = i
+			break
+		}
+
+		// ranges need to be merged
+		cr.Min = util.Min(r.Min, cr.Min)
+		cr.Max = util.Max(r.Max, cr.Max)
+	}
+
+	if breakIndex == -1 {
+		// Never broke the above loop; only `cr` to be appended
+		newCovered = append(newCovered, cr)
+	} else {
+		newCovered = append(newCovered, sm.Covered[breakIndex:]...)
+	}
+
+	sm.Covered = newCovered
+}
+
+func PartTwo() any {
+	sensors := getPartOneData()
+	sensorMap := NewSensorMap(4_000_001) // {max coordinate}+1
+
+	var distressX int
+	for t, s := range sensors {
+		distressX = t
+		sensorMap.TrySet(s.Point[0], s.Point[1], s.Point[1])
+		sensorMap.TrySet(s.Beacon[0], s.Beacon[1], s.Point[1])
+
+		beaconlessRange := ManhattanDist(s.Point, s.Beacon)
+		for xOffset := 0 - beaconlessRange; xOffset <= beaconlessRange; xOffset++ {
+			sensorMap.TrySet(
+				s.Point[0]+xOffset,
+				s.Point[1]+util.Abs(xOffset)-beaconlessRange,
+				s.Point[1]+beaconlessRange-util.Abs(xOffset),
+			)
+		}
+	}
+
+	distressX, distressY := -1, -1
+	if len(sensorMap.Covered) > 2 {
+		panic("too many ranges")
+	}
+	coord := sensorMap.Covered[0].Max + 1
+	distressX, distressY = coord/sensorMap.MaxLen, coord%sensorMap.MaxLen
+	return distressX*4_000_000 + distressY
+}
+
+func main() {
+	log.Infow(fmt.Sprintf("Answer: %v", PartOne()), "part", 1)
+	log.Infow(fmt.Sprintf("Answer: %v", PartTwo()), "part", 2)
+}
